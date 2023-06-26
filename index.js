0d76260fd6e2980fc5e6f3a40e82cc3e8bb82f2b
NAWAZ-SRM <115914519+NAWAZ-SRM@users.noreply.github.com>
2023-06-26 13:13:52 +0530

    Update index.js

--- index.js
+++ index.js
@@ -14,11 +14,11 @@ img2.setAttribute("src", randomSrc2);
 
 
 if (randomNumber1>randomNumber2) {
-    document.querySelector("h1").innerHTML="Player 1 wins";
+    document.querySelector("h1").innerHTML="&#128681 Player 1 wins";
 } 
 else if(randomNumber2>randomNumber1){
-    document.querySelector("h1").innerHTML="Player 2 wins";
+    document.querySelector("h1").innerHTML="Player 2 wins &#128681";
 }
 else{
-    document.querySelector("h1").innerHTML="It's a Draw";
+    document.querySelector("h1").innerHTML="&#128681 It's a Draw &#128681";
 }
